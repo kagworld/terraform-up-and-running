832278cf34f8ef0e0053b63c4c2a54255cfcb8b7
Khoa Nguyen <khoa@nguyen.com>
2023-01-17 22:23:41 +1100

    start web server on port 8080 in provisioned ec2 instance
    open port 8080 to allow web server to accept traffic

--- main.tf
+++ main.tf
@@ -7,4 +7,26 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami           = "ami-006fd15ab56f0fbe6"
   instance_type = "t3.nano"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+    #!/bin/bash
+    echo "Hello, World" > index.html
+    nohup busybox httpd -f -p 8080 &
+    EOF
+
+  tags = {
+    Name = "terraform-example"
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
