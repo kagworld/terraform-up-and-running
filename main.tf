32580c2a65c7d8abd13bcf6513aded4c69926fce
Khoa Nguyen <khoa@nguyen.com>
2023-01-18 15:51:18 +1100

    assign default web server port to 8080 because any port less than 1024 requires root user privileges. It's best practice to run web server as a non-root user with limited permission.

--- main.tf
+++ main.tf
@@ -34,6 +34,7 @@ resource "aws_security_group" "instance" {
 variable "server_port" {
   description = "The port the server will use for HTTP requests"
   type = number
+  default = 8080
 }
 
 output "public_ip" {
